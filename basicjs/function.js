278102b849f9dce46a652adf9f07b344d8b7b620
Arafat Patwary <farabipsk@gmail.com>
2024-08-03 11:16:27 +0600

    List of Function with this keyword

--- basicjs/function.js
+++ basicjs/function.js
@@ -1,8 +1,25 @@
 
-let q = myFunction(22,33);
+// let q = myFunction(22,33);
 
-function myFunction(a,b) {
-    return a * b;
-}
+// function myFunction(a,b) {
+//     return a * b;
+// }
 
-myFunction();
+// myFunction(); 
+
+//  project 
+//  function  toCelcias(){
+//     return (5/9) * (fahrenheit -32);
+//  }
+
+//  let data = toCelcias(77);  
+
+const person = {
+    firstName: 'Rayan',
+    lastName: 'Dhal',
+    id: '4356',
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    }
+
+};
