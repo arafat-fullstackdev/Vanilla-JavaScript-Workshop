7e0ca38266acddd3edc1746bf5d66bbd0e0a066a
Arafat Patwary <farabipsk@gmail.com>
2024-08-04 23:30:26 +0600

    Project Complete

--- basicjs/const_project.js
+++ basicjs/const_project.js
@@ -33,4 +33,50 @@ function Order(customer){
         this.calculateTotal();
     };
 
+    this.calculateTotal = function(){
+        this.totalAmount = this.items.reduce((total, item) => {
+            return total + (item.product.price * item.quantity);
+        }, 0);
+    };
+
+    this.getOrderSummery = function(){
+        return{
+            customer: this.customer.name,
+            items: this.items.map(item => ({
+                product: item.product.name,
+                quantity: item.quantity,
+                total: item.product.price * item.quantity
+
+            })),
+            status: this.status,
+            totalAmount: this.totalAmount
+            
+        };
+    };
+
+    this.updateStatus = function(newStatus){
+        this.status = newStatus;
+    };
+
 }
+
+//make product
+
+const product_One = new Product("Laptop", 10000, "Electronics");
+const product_Two = new Product("Ipad", 15000, "Electronics");
+
+//Make Customer
+
+const customer_One = new Customer("Efentov", "efettov@gmail.com");
+
+//Make Order
+const order_One = new Order(customer_One);
+order_One.addItem(product_One, 1);
+order_One.addItem(product_One, 2);
+
+//Order Summery
+console.log(order_One.getOrderSummery);
+
+//Order Status
+order_One.updateStatus('shipped');
+console.log(order_One.status);
