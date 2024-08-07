3051e991a6c8f36562d252a34949d5323494c19d
Arafat Patwary <farabipsk@gmail.com>
2024-08-07 15:26:56 +0600

    Promise with then

--- basicjs/Rectangle.js
+++ basicjs/Rectangle.js
@@ -1,16 +1,16 @@
-function Rectangle(width, height){
-    this.width = width;
-    this.height = height;
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
 
-    this.area = function(){
-        return this.width * this.height;
-    };
+  this.area = function () {
+    return this.width * this.height;
+  };
 
-    this.perimeter = function(){
-        return 2*(width + height);
-    };
+  this.perimeter = function () {
+    return 2 * (width + height);
+  };
 }
 
-const rect_one = new Rectangle(10 ,5);
+const rect_one = new Rectangle(10, 5);
 console.log(rect_one.area());
 console.log(rect_one.perimeter());
