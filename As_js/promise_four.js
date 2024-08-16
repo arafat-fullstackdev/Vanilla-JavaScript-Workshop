c062dc8fd4a20676649147ccadfb540b388fc46b
Arafat Patwary <farabipsk@gmail.com>
2024-08-16 09:31:01 +0600

    js event

--- As_js/promise_four.js
+++ As_js/promise_four.js
@@ -1,19 +1,30 @@
-let promise = new Promise(function(res, rej) {
-    setTimeout(function(){
-        const a = 'GreeksForGreeks';
-        const b = 'GreeksForGreeks';
+// let promise = new Promise(function(res, rej) {
+//     setTimeout(function(){
+//         const a = 'GreeksForGreeks';
+//         const b = 'GreeksForGreeks';
     
-        if(a == b){
-            res();
-        }else{
-            rej();
-        }
-    }, 5000);
+//         if(a == b){
+//             res();
+//         }else{
+//             rej();
+//         }
+//     }, 5000);
  
-});
+// });
 
-promise.then(function() {
-    console.log('Success! You are Greek!');
-}).catch(function(){
-    console.log('Try again! Some error has occurred!');
-});
+// promise.then(function() {
+//     console.log('Success! You are Greek!');
+// }).catch(function(){
+//     console.log('Try again! Some error has occurred!');
+// }); 
+
+//Example One
+
+let myPromise = new Promise(function(rest, fest){
+    rest('Hey! Lets take Coffee! ');
+})
+myPromise.then(function (successMsg){
+    console.log(successMsg);
+}), (function (errorMsg){
+    console.log(errorMsg);
+})
