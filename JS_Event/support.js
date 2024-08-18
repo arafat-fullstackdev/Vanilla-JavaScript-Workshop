70ec4899705fd0fb24c52e8ba1f549f579f084f1
Arafat Patwary <farabipsk@gmail.com>
2024-08-18 15:48:15 +0600

    use multiple function

---
+++ JS_Event/support.js
@@ -0,0 +1,14 @@
+function mesAge() {
+  alert("Hey Learner! Welcome Back");
+}
+mesAge();
+
+function onLoad() {
+  document.write("Frontend Engineer ");
+}
+onLoad();
+
+function windowWrite() {
+  document.write("web application");
+}
+windowWrite();
