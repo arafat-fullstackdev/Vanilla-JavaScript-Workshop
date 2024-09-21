353f7922f684f985b50e2d487d4674d98766df53
Arafat Patwary <farabipsk@gmail.com>
2024-09-21 11:06:49 +0600

    Add control structure

---
+++ JavaScript Basic/ControlStructure.js
@@ -0,0 +1,18 @@
+let logRegister = false;
+
+if(logRegister){
+    console.log('Please give your vote!');
+
+}else{
+    console.log('Please register!');
+}
+
+const trafficLight = 'Green';
+
+if(trafficLight === 'Red'){
+    console.log('Stop!');
+}else if(trafficLight === 'Green'){
+    console.log('Ongoing!');
+}else if(trafficLight === 'Yellow'){
+    console.log('Prepare to Start!');
+}
