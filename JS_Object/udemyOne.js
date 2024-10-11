c19659259fffcf729281e61b4b04ed58df86d0f9
Arafat Patwary <farabipsk@gmail.com>
2024-10-11 11:14:01 +0600

    JS Object properties

---
+++ JS_Object/udemyOne.js
@@ -0,0 +1,10 @@
+// js object literals
+
+const book  = {
+    title: 'The Lord of The Kings',
+    Author: 'JK Ronald',
+    price: '$16',
+    available: true, //Boolean Value
+    pages: '354'
+}
+console.log(book);
