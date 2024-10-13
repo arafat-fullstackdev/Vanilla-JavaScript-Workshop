a7427aae84359f00254ff327b93f1a059b0e037a
Arafat Patwary <farabipsk@gmail.com>
2024-10-13 16:46:01 +0600

    JS Concat Array

--- JS_Object/udemyOne.js
+++ JS_Object/udemyOne.js
@@ -8,3 +8,25 @@ const book  = {
     pages: '354'
 }
 console.log(book);
+
+const bookTwo = {
+    title:'The Bermuda Triangle',
+    writer: 'Dave Hopes',
+    price: '$36',
+    available: true, 
+}
+
+const bookTitle = bookTwo['title'];
+console.log(bookTitle);
+
+const employee = {
+    name:'Brandon Tom ',
+    id: '05'
+}
+
+employee.address ={
+   city: 'Comilla',
+    road: 'Jawtalla'
+}
+
+console.log(employee.address.road);
