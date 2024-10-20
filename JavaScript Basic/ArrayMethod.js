9f5cbd0ce6e4ee984e64de877dac501f5c45200b
Arafat Patwary <farabipsk@gmail.com>
2024-10-20 23:16:04 +0600

    Add Fun objection with function

--- JavaScript Basic/ArrayMethod.js
+++ JavaScript Basic/ArrayMethod.js
@@ -6,4 +6,22 @@ console.log(person);
 
 const person1 = person;
 person1.age= 22;
-console.log(person1);
+person1.name= 'Brain';
+
+let modify = {
+    name: 'Daniel',
+    age: 44
+};
+person1.age = modify.age;
+// console.log(person1);
+// console.log(person); 
+const funObj = (aunAge)=> {
+    funObj.age += 1;
+}
+const Ponting = {
+    name: 'Ricky',
+    age: 11
+}
+console.log('before', Ponting);
+funObj(Ponting);
+console.log('after', Ponting);
