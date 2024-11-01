02066c1e28bcf0f28d6325fcb59837e4fa6d7f08
Arafat Patwary <farabipsk@gmail.com>
2024-11-01 16:52:01 +0600

    Window object revise

---
+++ WOM/basic_windowObj.js
@@ -0,0 +1,7 @@
+//? window Object //
+// console.log(window.Object);
+// console.log(window.innerHeight);
+// console.log(window.location);
+// console.log(window.history);
+// console.log(window.location.host);
+console.log(window.navigator);
