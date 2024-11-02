8ae010b6780482c690be4535f01c5721fda1baa2
Arafat Patwary <farabipsk@gmail.com>
2024-11-02 15:22:44 +0600

    Use selector

--- WOM/basic_windowObj.js
+++ WOM/basic_windowObj.js
@@ -4,4 +4,16 @@
 // console.log(window.location);
 // console.log(window.history);
 // console.log(window.location.host);
-console.log(window.navigator);
+
+// console.log(window.navigator);
+// console.log(window.screen);
+// console.log(window.alert('Hey!'));
+// console.log(window.confirm());
+// window.prompt() 
+
+// window.open('https://www.udemy.com/'); 
+
+//* query selector*//
+
+const laptopEl = document.querySelector("[data-rating='4.7']");
+console.log(laptopEl);
