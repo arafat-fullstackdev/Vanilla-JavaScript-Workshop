6203caa27d970842753863d36027ca4af7e239fd
Arafat Patwary <farabipsk@gmail.com>
2024-11-05 22:02:32 +0600

    check data type

--- JS_InterviewQ/index.js
+++ JS_InterviewQ/index.js
@@ -2,3 +2,6 @@ console.log('5' + 3);
 console.log(5 + 3);
 console.log('5' - 3);
 console.log(5 - 3);
+
+console.log('5' == 5); //check only value
+console.log('5' === 5); // check value strictly with type of data.
