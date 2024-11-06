bafdc6198e6da81902e804f84f44e1293ab57f58
Arafat Patwary <farabipsk@gmail.com>
2024-11-06 10:03:13 +0600

    use multiple string methods

--- JS_InterviewQ/index.js
+++ JS_InterviewQ/index.js
@@ -1,7 +1,13 @@
-console.log('5' + 3);
-console.log(5 + 3);
-console.log('5' - 3);
-console.log(5 - 3);
+// console.log('5' + 3);
+// console.log(5 + 3);
+// console.log('5' - 3);
+// console.log(5 - 3);
 
 console.log('5' == 5); //check only value
-console.log('5' === 5); // check value strictly with type of data.
+console.log('5' === 5); // check value strictly with type of data. 
+let singleString = ('Hello programmer!');
+console.log(singleString);
+let doubleString = ('Nice to meet you!');
+console.log(doubleString);
+let bactickString = (`Hello Coder!`);
+console.log(bactickString);
