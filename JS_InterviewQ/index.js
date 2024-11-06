c43f0f0d9b9961d43888f85e395726a8d35f3ac4
Arafat Patwary <farabipsk@gmail.com>
2024-11-06 16:07:25 +0600

    use string in console log

--- JS_InterviewQ/index.js
+++ JS_InterviewQ/index.js
@@ -3,11 +3,13 @@
 // console.log('5' - 3);
 // console.log(5 - 3);
 
-console.log('5' == 5); //check only value
-console.log('5' === 5); // check value strictly with type of data. 
+// console.log('5' == 5); //check only value
+// console.log('5' === 5); // check value strictly with type of data. 
 let singleString = ('Hello programmer!');
 console.log(singleString);
 let doubleString = ('Nice to meet you!');
 console.log(doubleString);
 let bactickString = (`Hello Coder!`);
 console.log(bactickString);
+let stringObj = new String('Aljazeera news from Doha');
+console.log(stringObj);
