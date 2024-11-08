34893950c9acb4cb2e14b54bbd95715b5a0c5abc
Arafat Patwary <farabipsk@gmail.com>
2024-11-08 18:54:17 +0600

    Code with Greeks

--- JS_InterviewQ/index.js
+++ JS_InterviewQ/index.js
@@ -5,11 +5,14 @@
 
 // console.log('5' == 5); //check only value
 // console.log('5' === 5); // check value strictly with type of data. 
-let singleString = ('Hello programmer!');
-console.log(singleString);
-let doubleString = ('Nice to meet you!');
-console.log(doubleString);
-let bactickString = (`Hello Coder!`);
-console.log(bactickString);
-let stringObj = new String('Aljazeera news from Doha');
-console.log(stringObj);
+// let singleString = ('Hello programmer!');
+// console.log(singleString);
+// let doubleString = ('Nice to meet you!');
+// console.log(doubleString);
+// let bactickString = (`Hello Coder!`);
+// console.log(bactickString);
+// let stringObj = new String('Aljazeera news from Doha');
+// console.log(stringObj);
+
+//? this questions from Greeks //
+ console.log("Hi there i am from Second Space\n i  will teach you Algorithem");
