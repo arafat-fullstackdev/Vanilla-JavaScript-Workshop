c96a161856d8718510e6e189ef13b54613f00675
Arafat Patwary <farabipsk@gmail.com>
2024-11-11 20:57:01 +0600

    find repeat

--- JS_InterviewQ/chatGpt.js
+++ JS_InterviewQ/chatGpt.js
@@ -12,3 +12,29 @@ function codeReverse(btr){
     return btr.split('').reverse('').join('');
 }
 console.log(codeReverse('Hey!'));
+
+// Problem: Write a function that returns 
+//the first non-repeating character 
+//in a string. If none exists, return null.
+
+function firstNonRepeat(str){
+     const charCount = {};
+
+
+for(let char of str){
+    charCount[char] = (charCount[char] || 0) +1;
+}
+
+// find 1st char with  count 1
+for(let char of str){
+    if(charCount[char] === 1){
+        return char;
+    }
+}
+
+return null;
+}
+
+console.log(firstNonRepeat('swiss'));
+console.log(firstNonRepeat('aabbcc'));
+console.log(firstNonRepeat('sggghh'));
