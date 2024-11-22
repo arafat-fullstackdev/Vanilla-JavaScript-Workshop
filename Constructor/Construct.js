5b241b84d29d60a2ea9d4a4ac2c21ca9295dfc54
Arafat Patwary <farabipsk@gmail.com>
2024-11-22 20:19:30 +0600

    Add JS Constructor

---
+++ Constructor/Construct.js
@@ -0,0 +1,9 @@
+//? JS Constructor //
+class coder{
+    constructor(stack, platform){
+        let solver = 'hackerRank' + 'codeForce'; 
+        console.log(stack,platform);
+    }
+}
+
+let programmer = new coder('CodeCheaf', 'LeetCode');
--- JS_InterviewQ/function.js
+++ JS_InterviewQ/function.js
@@ -1,8 +1,14 @@
 // create a function 
 function abc(a,b){
+
+    "use strict"
     //let a = 120, b = 220;
     return arguments[0] + arguments[1];
     console.log(a + b);
 }
 
 console.log(abc(12, 44));
+
+var ds = 23;
+var nb = 44;
+console.log(window.ds + window.nb);
