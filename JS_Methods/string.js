8647610d02cf6ef77ddaba29e54d05381e6247b2
Arafat Patwary <farabipsk@gmail.com>
2024-11-26 16:21:57 +0600

    Add multiple methods

--- JS_Methods/string.js
+++ JS_Methods/string.js
@@ -1,4 +1,26 @@
+//? JS CharAt(index) Methods //
 let str = "javaScript";
-console.log(str.charAt(1));
-console.log(str.charAt(0));
-console.log(str.charAt(2));
+// console.log(str.charAt(1));
+// console.log(str.charAt(0));
+// console.log(str.charAt(2));
+let btr = "Hey! Python NIce to meet you";
+let rty = "Thanks for joining me";
+
+//! include(searchString) Methods //
+let drt = "Dart Java";
+// console.log(btr.charAt(3));
+// console.log(btr.charAt(4));
+// console.log(btr.charAt(5));
+console.log(drt.includes("Java"));
+
+//* replace(searchValue, newValue) methods //
+let vtr = "Hello Flutter!";
+console.log(vtr.replace("Flutter", "JS"));
+
+//? slice(start, end) methods //
+let code = "CodeChef";
+console.log(code.slice(1, 4));
+console.log(code.slice(0, 4));
+//* uppercase() methods //
+let dyn = "javascript";
+console.log(dyn.toUpperCase());
