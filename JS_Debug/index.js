79e651cb2cce434306b44f3472dbda3ae2a0e5c7
Arafat Patwary <farabipsk@gmail.com>
2024-12-08 21:07:20 +0600

    Fetching fata from server

---
+++ JS_Debug/index.js
@@ -0,0 +1,4 @@
+const inputName = document.querySelector("#inputName");
+const inputGrade = document.querySelector("#inputGrade");
+const btnSend = document.querySelector("#btnSend");
+const showRes = document.querySelector("#showRes");
