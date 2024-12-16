4f3ba3cb8d40b9354a15c866d0e17f3cfbe5a306
Arafat Patwary <farabipsk@gmail.com>
2024-12-16 21:37:22 +0600

    Add Node parent

---
+++ DOM/index.js
@@ -0,0 +1,11 @@
+//? parentNode  When To use ? //
+// Manipulating a child parent.
+//Navigate from deeply elements.
+// Code needs to be generic and independent of IDS or classes.
+let a = 'NASA';
+let b = 'SpaceX';
+let c = 'Tesla';
+
+console.log(a)
+console.log(b)
+console.log(c)
