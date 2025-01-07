46fb92ea0d38ab0a19dc071d84e98ac2f36e80bc
Arafat Patwary <farabipsk@gmail.com>
2025-01-07 07:21:58 +0600

    key value in object

--- JS_Object/iterator.js
+++ JS_Object/iterator.js
@@ -1,12 +1,21 @@
-let userDetails = {
-    name: 'CodeBase',
-    age: 28,
-    isAdmin: true,
-    address: 'Comilla',
-}
+// let userDetails = {
+//     name: 'CodeBase',
+//     age: 28,
+//     isAdmin: true,
+//     address: 'Comilla',
+// }
 
-for(let Property in userDetails){
-    // Property names
-    //alert(Property);
+// for(let Property in userDetails){
+//     // Property names
+//     //alert(Property);
+// }
+// console.log(userDetails); 
+
+// Practice from MDN
+const object1 = {
+    a:'Something',
+    b: 39,
 }
-console.log(userDetails);
+for(const [Key, value] of Object.entries(object1)){
+    console.log(`${key}: ${value}`);
+}
