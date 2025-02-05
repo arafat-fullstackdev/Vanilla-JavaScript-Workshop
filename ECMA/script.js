4c0997eeeb7c5c11315ab5448d6ee3a2724ce5e4
Arafat Patwary <farabipsk@gmail.com>
2025-02-05 22:24:14 +0600

    use array splice and with

--- ECMA/script.js
+++ ECMA/script.js
@@ -2,7 +2,7 @@
 
 //*Orginal array
 const arr = [11,22,33,44,55];
-//Mutaable way
+//Mutable way
 // const arrayReverse = arr.reverse();
 
 // console.log(arrayReverse);
@@ -11,11 +11,23 @@ const arr = [11,22,33,44,55];
 //? Immutable way
 const arrayReverse = arr.toReversed();
 
-console.log(arrayReverse);
-console.log(arr);
+//console.log(arrayReverse);
+//console.log(arr);
 
 //* Array Splice()
 const arraySplice = [1,2,3,4,5,6];  
-const removeElement = arraySplice.splice(2,2,200,300,400); //? take 3 param = 1 = start index, 2= deleted element , 3= add element
-console.log(removeElement); //return deleted element
-console.log(arraySplice); // return modified element
+//const removeElement = arraySplice.splice(2,2,200,300,400); //? take 3 param = 1 = start index, 2= deleted element , 3= add element
+// reverse splice()
+//const removeSplice = arraySplice.splice(-2,2,200,700,800); //? take 3 param = 1 = start index, 2= deleted element , 3= add element
+//? Immutable way
+const splicedArray = arraySplice.toSpliced(-2,2,700,800); //? ES14
+//console.log(removeElement); //return deleted element
+//console.log(splicedArray); //return deleted element
+//console.log(arraySplice); // return modified element
+
+//**  Array with()
+const arrWith=[11,22,33,44];
+//? Immutable way
+const res = arrWith.with(1,55) // 1 = position; 55= add element
+console.log(res);
+console.log(arrWith);
