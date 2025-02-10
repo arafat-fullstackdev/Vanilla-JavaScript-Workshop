d74de41487132fe992490a32b20b3d077afc9999
Arafat Patwary <farabipsk@gmail.com>
2025-02-10 12:11:06 +0600

    Start Execuation context

---
+++ V8_Engine/index.js
@@ -0,0 +1,16 @@
+//? JS to Browser to Engine to Computer to output
+//* Machine code = Interpitation to Compliation
+//! Just In Time(JIT) = Compiler(MachineCode) 
+
+ function takeOrder()  {
+    
+    console.log(`Take time on order 0`);
+    console.log(`Take time on order 1`);
+    console.log(`Take time on order 2`);
+    console.log(`Take time on order 3`);
+    console.log(`Take time on order 4`);
+ 
+
+};
+takeOrder();
+    
