6bb7276b2c85f783f60cefb9795c237344415a65
Arafat Patwary <farabipsk@gmail.com>
2025-02-18 17:06:43 +0600

    USe regex

--- JS_Regex/script.js
+++ JS_Regex/script.js
@@ -1,10 +1,11 @@
 //*
 // W= word, D= digit, S= whiteSpace, C= character, 
 //regex = /\w/g;
-regex = /\D/g;
-//regex = /\s/g;
+//regex = /\D/g;
+regex = /color?r{2,4}/g;
+//regex = /[^a-lG-M]/g;
 
-var str= "Edit the Expression & Text to see matches. Roll over matches or the expression for details"
+var str= "Edit the Expression colorr colorrrr & Text torrr see matches.Clorerrr Rolrrl  colorr overu Coloreerrrr matches or the expression for details"
 
 const result = str.match(regex);
 console.log(result);
