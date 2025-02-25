27657f064471234f830c72e74d8966ac14835d57
Arafat Patwary <farabipsk@gmail.com>
2025-02-25 11:14:03 +0600

    USe dom for element sibling

---
+++ DOM/script.js
@@ -0,0 +1,9 @@
+// select a list of property
+const appleF = document.getElementById('apple');
+
+// select the previous sibling property
+const prev = appleF.previousSibling;
+const nextFruit = appleF.nextElementSibling;
+
+//console.log(prev);
+console.log(nextFruit);
