41abbd9ffa525bda9acb3e6dfc006ff0b4d84914
Arafat Patwary <farabipsk@gmail.com>
2025-03-01 16:46:57 +0600

    Add setAttribute

--- DOM/script.js
+++ DOM/script.js
@@ -11,9 +11,18 @@
 //* To DO App
 //console.dir(document);
 
-let headerElement = document.getElementById('title');
-//headerElement.textContent = 'To Do Application';
-//headerElement.innerText = 'To Do AppS';
+// let headerElement = document.getElementById('title');
+// //headerElement.textContent = 'To Do Application';
+// //headerElement.innerText = 'To Do AppS';
 
-console.log(headerElement.innerText);
-console.log(headerElement.textContent);
+// console.log(headerElement.innerText);
+// console.log(headerElement.textContent); 
+
+//? Set Attribute
+const Images = document.getElementById('randomImg')
+.querySelector("img");
+console.log(Images);
+Images.setAttribute("src",
+    "https://images.unsplash.com/photo-1518895949257-7621c3c786d7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww");
+
+console.log(Images);
