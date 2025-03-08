648e62a850b1b234085f44dede1fa27321893ed9
Arafat Patwary <farabipsk@gmail.com>
2025-03-08 23:41:21 +0600

    USE JSON data

---
+++ API/example.js
@@ -0,0 +1,41 @@
+// Identify Object
+/* USER:
+    -id:
+    -name:
+    -email:
+    -password:
+    -birthday:
+    -phone:
+
+    POST:
+       -"title"
+       -"body"
+       -"[comments]"
+       -"createTimes"
+       -"updateTimes"
+
+      
+ */
+
+// Create Model URI
+/*
+  https://examnple.com/users GET
+  https://examnple.com/users/ POST
+
+  https://examnple.com/users/id: GET
+  https://examnple.com/users/id: PUT/PATCH
+  https://examnple.com/users/id: DELETE
+
+
+  https://examnple.com/posts/ GET
+  https://examnple.com/posts/ POST
+
+  https://examnple.com/posts/id: GET
+  https://examnple.com/posts/id: PUT/PATCH
+  https://examnple.com/posts/id: DELETE
+
+*/
+
+//Determine Representation
+//Single data Determine Representation
+//JSON view
