7cfb73b74554d77c7adf826fda0c436c863648f1
Arafat Patwary <farabipsk@gmail.com>
2025-03-29 10:43:18 +0600

    Makew closer fun

---
+++ JS_Closer/script.js
@@ -0,0 +1,29 @@
+var num1 = 11;
+var num2 = 21;
+
+var sum =function add(){
+    return num1 * num2;
+}
+console.log(sum());
+
+function int(){
+    var name = 'JS Closer';
+
+    function displayName(){
+        console.log(name);
+    }
+    displayName();
+}
+
+int();
+
+console.log(int());
+//console.log(displayName());
+var x;
+if(Math.random() > 0.5){
+    var x = 2;
+}else{
+    var x = 4;
+}
+
+console.log(x);
