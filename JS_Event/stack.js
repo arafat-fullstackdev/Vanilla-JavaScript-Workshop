92947dfa6069311ffabc086ba6e76555a2ac5916
Arafat Patwary <farabipsk@gmail.com>
2025-04-22 23:31:22 +0600

    Update Buble event

--- JS_Event/stack.js
+++ JS_Event/stack.js
@@ -28,6 +28,14 @@
 //   }
   
 //   const baz = bar(7); // assigns 42 to baz
+
+
+
+// Call Stack: JavaScript has a call stack where function execution is managed in a Last-In, First-Out (LIFO) order.
+// Web APIs (or Background Tasks): These include setTimeout, setInterval, fetch, DOM events, and other non-blocking operations.
+// Callback Queue (Task Queue): When an asynchronous operation is completed, its callback is pushed into the task queue.
+// Microtask Queue: Promises and other microtasks go into the microtask queue, which is processed before the task queue.
+// Event Loop: It continuously checks the call stack and, if empty, moves tasks from the queue to the stack for execution.
   
 console.log('Start!');
 
