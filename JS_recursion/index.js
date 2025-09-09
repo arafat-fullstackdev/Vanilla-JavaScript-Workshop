b39240ccc6962e1612e855055965ac16c9eddb22
Arafat Patwary <farabipsk@gmail.com>
2025-09-09 21:04:27 +0600

    Add Recurssion feature

---
+++ JS_recursion/index.js
@@ -0,0 +1,9 @@
+function recursiveFunction(parameters) {
+  // Base case: stopping condition
+  if (baseCase) {
+    return baseCaseValue;
+  }
+
+  // Recursive case: function calls itself
+  return recursiveFunction(modifiedParameters);
+}
