f1e2f6a5a7292d800df9e58cb6cb2a05ca796ad8
Arafat Patwary <farabipsk@gmail.com>
2025-11-02 14:59:47 +0600

    Code practice immutable js

---
+++ JS_Debug/immutable.js
@@ -0,0 +1,12 @@
+// immutable 
+let  greet = "Hello ";
+greet += "JS"
+console.log(greet);
+ let  code = [1,2,3,4];
+ code.push(8);
+ 
+//  console.log(code);
+  let code1 = [...code,10];
+ console.log("Update",code1)
+  console.log(code);
+
