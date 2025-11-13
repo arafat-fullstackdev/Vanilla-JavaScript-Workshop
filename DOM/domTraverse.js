7ec3146cdb80e50d2c848dc94f92a8b74dc18a2f
Arafat Patwary <farabipsk@gmail.com>
2025-11-13 11:04:53 +0600

    Traverse DOM

---
+++ DOM/domTraverse.js
@@ -0,0 +1,7 @@
+// console.dir(document)  
+// console.dir(typeof(document))
+console.log(document.title);
+console.dir(document.URL);
+console.dir(document.domain);
+console.dir(document.head)
+document.title="Play With DOM!"
