e8ba93b271aef821ae9acf2ed0a27aab71903fc5
Arafat Patwary <farabipsk@gmail.com>
2025-11-13 15:14:14 +0600

    Traverse DOM element

--- DOM/domTraverse.js
+++ DOM/domTraverse.js
@@ -1,7 +1,28 @@
-// console.dir(document)  
-// console.dir(typeof(document))
-console.log(document.title);
-console.dir(document.URL);
-console.dir(document.domain);
-console.dir(document.head)
-document.title="Play With DOM!"
+// // console.dir(document)  
+// // console.dir(typeof(document))
+// console.log(document.title);
+// console.dir(document.URL);
+// console.dir(document.domain);
+// console.dir(document.head)
+// document.title="Play With DOM!"
+
+//console.log(document.all)
+//console.log(document.all.filter())
+
+// console.log(document.images)
+// console.log(document.links)
+// console.log(document.forms)
+
+// //? DOM travers with loop
+// for(let element of document.all){
+//     console.log(element)
+// }
+
+let hederElement = document.getElementById('header');
+//hederElement.textContent= 'Play With DOM!';
+//hederElement.innerText= 'Play With DOM!';
+// console.log(hederElement.innerText); // Browser final output
+// console.log(hederElement.textContent);// Editor final output
+// console.log(hederElement.innerHTML);// Editor final output
+console.dir(hederElement);
+hederElement.style.color= 'green';
