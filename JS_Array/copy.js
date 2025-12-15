d8269ffcd952f304928c8251a9d67e5a86edbd5d
Arafat Patwary <farabipsk@gmail.com>
2025-12-15 11:46:55 +0600

    Practice JS Array and Object DS

--- JS_Array/copy.js
+++ JS_Array/copy.js
@@ -37,7 +37,7 @@ const car={
     year: 2025,
 }
 const carObject = Object.values(car);
-console.log(carObject);
+//console.log(carObject);
 
 //! Property check
 const hasProperty = car.hasOwnProperty("brand");
@@ -48,7 +48,7 @@ const hasProperty = car.hasOwnProperty("brand");
 const targetObj = {name: "Captain Bon"};
 const sourceObj = {country: "marse23"};
 const result = Object.assign(targetObj,sourceObj);
-console.log(result)
+//console.log(result)
 // Object.assign(EventTarget, source)
 
 //! Delete
@@ -60,4 +60,130 @@ console.log(result)
 
 // modify
 car.make="Nissian";
-console.log(car)
+//console.log(car)
+
+//? Nesting Obj
+
+const developer={
+    role: "FullStack",
+    stack: 'MERN',
+    learningP: "Udemy",
+    engineering:{
+        role: 'Logic Building',
+        stack: "Fullstack",
+        learningP: 'Techno'
+    }
+}
+
+//console.log(developer);
+
+//? Access nested element
+const userRole = developer.engineering;
+//console.log(userRole)
+
+//* Object an Array
+const userProfile= [
+    {
+        name: "Bob",
+        stack: 'MERN',
+        role: 'Developer'
+        
+    },
+    {
+        name: 'Alison',
+        stack: 'Lamp',
+        role: 'engineer'
+    },
+    {
+        name: 'Hololo',
+        stack: 'QE',
+        role: 'Q'
+    }]
+//console.log(userProfile)
+let firstProfile = userProfile[0];
+//console.log(firstProfile)
+//!Add object by push method
+userProfile.push({name: "Deny", stack: "Python", role: 'Backend'});
+
+//! modify 
+const modifyData= userProfile[0].name= 'Bon';
+//console.log(userProfile)
+
+//? Array transformation
+const arrayL = ['1','2','3','4'];
+arrayL.forEach(function(n){
+    let double = n *2;
+  //console.log(double);
+})
+
+const products = [
+    {name: 'laptop', stock: 4, price: 2000},
+    {name: 'desktop', stock: 14,  price:2300},
+    {name: 'tablet', stock: 10,  price: 2200},
+];
+
+// product.forEach((seperateP)=>{
+//    // console.log(`Product Update: ${seperateP.name} and ${seperateP.stock}`);
+// })
+ const discountprice= products.map((product)=>{
+    return{
+       name: product.name,
+       stock: product.stock,
+       price: product.price * 0.9
+    }
+})
+
+//console.log(discountprice)
+
+//! Array with map()
+let coder =['JS','C','C++','Java','Python'];
+
+coder.map( function (code){
+//console.log(code);
+})
+
+const numberArray = [2,4,6,8];
+
+const doubleElements = numberArray.map(function (num){
+  //console.log(num)
+  return num *3;
+})
+
+//console.log(doubleElements)
+
+//! reduce function
+
+let number1 = [1,2,3,4,5,6];
+
+const totalNum= number1.reduce(function(accumulator, currentValue){
+//   console.log('acc',accumulator);
+//   console.log('value', currentValue)
+
+return accumulator +currentValue;
+},0)
+
+//console.log(totalNum)
+
+const products2 = [
+    {name: 'laptop', stock: 4, price: 2000, isStock: false},
+    {name: 'desktop', stock: 14,  price:2300, sStock:true},
+    {name: 'tablet', stock: 10,  price: 2200, sStock:true},
+];
+
+ const totalValue= products2.reduce(function(acc, product){
+    // console.log(acc);
+    // console.log(curr);
+
+    return acc + product.stock + product.price
+
+},0)
+
+//console.log(totalValue)
+
+//? find()
+let number3 =  [5,10,20,25];
+const findValue = number3.find(function (num){
+    return num> 10;
+})
+
+console.log(findValue)
