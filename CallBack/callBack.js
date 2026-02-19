c66e959b9870e39fe440cf6505fffc00d815d7c5
Arafat Patwary <farabipsk@gmail.com>
2026-02-19 16:40:33 +0600

    Create IIF

---
+++ CallBack/callBack.js
@@ -0,0 +1,10 @@
+function greet(name, callBack){
+    console.log('Hello!, '+ name);
+    //callBack();
+};
+
+function sayHello(){
+    console.log("Let's start")
+}
+
+greet('Rayan dal', sayHello);
---
+++ JS_Function/simple.js
@@ -0,0 +1,15 @@
+//IIF Immediately Invoked Function
+// (function (){
+//     console.log('Hello Fungo!');
+// })()
+
+// (function(m,n){
+//   let count= m +n;
+//   console.log(count)
+// })(4,8)
+
+// Arrow
+((a,b)=>{
+    let res = a +b;
+    console.log(res)
+})(5,5)
