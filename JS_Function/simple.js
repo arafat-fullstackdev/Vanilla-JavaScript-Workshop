954efd10e4fd227af9cf9d2bf24986d40e4b48db
Arafat Patwary <farabipsk@gmail.com>
2026-02-21 12:20:13 +0600

    Make string formator

--- JS_Function/simple.js
+++ JS_Function/simple.js
@@ -24,29 +24,72 @@
 //? Simple Calculate
 
 // const add = (a,b)=>{
-//    const result = a + b;
+//    let result = a + b;
 //    return result;
    
 // }
 // console.log(add(4,6));
 
 // const sub = (a,b)=>{
-//    const result = a - b;
+//    let result = a - b;
 //    return result;
    
 // }
 // console.log(sub(40,6));
 
 // const multi = (a,b)=>{
-//    const result = a * b;
+//    let result = a * b;
 //    return result;
    
 // }
 // console.log(multi(4,6)); 
 
-const div = (a,b)=>{
-   const result = a + b;
-   return result;
+// const div = (a,b)=>{
+//    let result = a + b;
+//    return result;
    
+// }
+// console.log(div(40,6));
+
+//** car */
+
+// function characterCount(text){
+//   let countText= text.length;
+//   return countText;
+
+// }
+//  const textChar= characterCount("JavaScript")
+//  console.log(textChar)
+ 
+
+//  function charLowerCase(text){
+//   let lowerText =text.toLowerCase();
+//   return lowerText;
+//  }
+
+//  console.log(charLowerCase('JAVASCRIPT'))
+
+//  function charUpperCase(text){
+//   let upperCase= text.toUpperCase();
+//   return upperCase;
+//  }
+//  console.log(charUpperCase('nodejs'))
+
+//  const slicedText= function(txt,start,end){
+//   let sliced = txt.slice(start,end);
+//   return sliced;
+//  }
+
+//   let slicedCode= slicedText('MongoDB', 0,6);
+//   console.log(slicedCode)
+
+//?String formate
+
+function formatString(str1,str2){
+  let upperString= str1.toUpperCase();
+  let lowerString= str2.toLowerCase();
+  let combined= upperString + " " + lowerString;
+  return combined;
 }
-console.log(div(40,6));
+let modify = formatString("Express", "JS");
+console.log(modify)
